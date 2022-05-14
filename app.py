d51e2e12f3e5ff1c834bde7b7b740d2b0cbc3783
Chris Kimani <62855363+Chriskimani@users.noreply.github.com>
2022-05-14 15:47:45 +1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+# source venv/bin/activate  will start the virtual environment for this app. #
+# use code below to run the app on a dev server
+
+# export FLASK_APP=app.py      
+# export FLASK_ENV=development
+# flask run
+
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+
+def create_app():
+	app = Flask(__name__)
+	client = MongoClient("mongodb+srv://Ckimani:5Gg6bTwH6nzsEtqW@microblog-application.f3ij4.mongodb.net/test")
+	app.db =  client.microblog # this is not good practice
+
+
+	@app.route('/', methods=['GET', 'POST']) # this is the route for this particular page - is it oop so i don't have to build all pages here though? 
+	def home():
+		if request.method == 'POST':
+			entry_content = request.form.get("content")
+			formatted_date =  datetime.datetime.today().strftime("%Y-%m-%d")
+			app.db.entries.insert({"content": entry_content, "date":formatted_date})
+
+		entries_with_date = [
+			(
+				entry["content"], 
+				entry["date"],
+				datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime('%b %d')
+			)
+			for entry in app.db.entries.find({})
+		]
+		return render_template("home.html", entries=entries_with_date)
+	return app
