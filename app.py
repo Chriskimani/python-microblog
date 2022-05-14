e38a09a2c0f8b81adca962970b0f9000c7e9b204
Chris Kimani <62855363+Chriskimani@users.noreply.github.com>
2022-05-14 17:16:14 +1000

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,18 @@
 # export FLASK_ENV=development
 # flask run
 
+import os 
 import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_app():
 	app = Flask(__name__)
-	client = MongoClient("mongodb+srv://Ckimani:5Gg6bTwH6nzsEtqW@microblog-application.f3ij4.mongodb.net/test")
+	client = MongoClient(os.environ.get("MONGODB_URI"))
 	app.db =  client.microblog # this is not good practice
 
 
